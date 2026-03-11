6392295a5c039edffad37d8b2856cccc95ee2b59
11aki2358 <f331dpprpl@gmail.com>
2026-03-11 12:03:40 +0900

    first commit

--- api/index.php
+++ api/index.php
@@ -1,14 +1,14 @@
 <?php
 
-// $url = "https://scrapbox.io/api/pages/testblog11aki2358/";
-// // 新しい cURL セッションを初期化します
-// // コネクションを開く  
-// $ch = curl_init(); // はじめ
+$url = "https://scrapbox.io/api/pages/testblog11aki2358/";
+// 新しい cURL セッションを初期化します
+// コネクションを開く  
+$ch = curl_init(); // はじめ
 
-// //オプション
-// curl_setopt($ch, CURLOPT_URL, $url); 
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// $html =  curl_exec($ch);
-// var_dump($html);
+//オプション
+curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$html =  curl_exec($ch);
+var_dump($html);
 
-phpinfo();
+// phpinfo();
