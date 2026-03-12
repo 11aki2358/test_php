ab8e6e77104200296437e9ea598b6e23a597ecd5
11aki2358 <f331dpprpl@gmail.com>
2026-03-12 11:53:38 +0900

    blog: test Scrapbox API

--- api/index.php
+++ api/index.php
@@ -88,6 +88,34 @@
           ブログ一覧に表示する
         </li>
       </ul>
+
+      <?php
+      $url_tags = "https://scrapbox.io/api/pages/ryko-ryko/tags";
+      // 新しい cURL セッションを初期化します
+      // コネクションを開く
+      $ch_tags = curl_init(); // はじめ
+      
+      //オプション
+      curl_setopt($ch_tags, CURLOPT_URL, $url_tags);
+      curl_setopt($ch_tags, CURLOPT_RETURNTRANSFER, true);
+      $html_tags = curl_exec($ch_tags);
+      $decodedResults_tags = json_decode($html_tags);
+      
+      // タイトルを表示
+      $lines = $decodedResults_tags->lines;
+
+
+      for($i = 1; $i < count($lines); $i++){
+        $tag_name = preg_replace('/\[|\]/', "", $lines[$i]->text);
+
+        echo("<p>");
+        echo($tag_name);
+        echo("</p>");
+      }
+
+
+      ?>
+
     </div>
 
 
@@ -152,7 +180,7 @@
           if (!$pages[$i]->pin) {
             //  ピン止めされている投稿は除外
             //  top, setting, タグの説明など
-
+        
 
             // 個ページについて
             $single_url = ("https://scrapbox.io/api/pages/ryko-ryko/" . $title);
@@ -168,6 +196,7 @@
             // タイトルを表示
             $decodedResults_single = json_decode($html_single);
 
+            // $descriptions = $decodedResults_single->descriptions;
             $lines = $decodedResults_single->lines;
 
             echo ("<div class=\"blog-article\">\n");
@@ -175,12 +204,160 @@
             echo ($title);
             echo ("</h2>");
 
-            for ($j = 1; $j < count($lines); $j++) {
-              echo ("<p>");
+            $j = 1;
+            while ($j <= 5) {
+
+              if (!strcmp("...", $lines[$j]->text)) {
+                //  "..." 以降の行はタグ情報なので、表示しない
+                break;
+              }
+
+              if (preg_match('/\[http\S*\]/', $lines[$j]->text)) {
+                //  [http...]
+                //  URLを含む場合
+                //  リンク名は不明
+                echo ("<p class=\"red\">");
+              } else {
+                echo ("<p>");
+              }
+
+
+
               echo ($lines[$j]->text);
               echo ("</p>");
+              $j++;
+
             }
 
+            if (count(($lines)) > 6) {
+              echo ("<details>");
+              echo ("<summary>");
+              echo ("show more");
+              echo ("</summary>");
+              $j = 6;
+
+              while ($j < count($lines)) {
+
+                if (!strcmp("...", $lines[$j]->text)) {
+                  //  "..." 以降の行はタグ情報なので、表示しない
+                  break;
+                }
+
+                if (preg_match('/\[https:\/\/gyazo.com\/\S*\]/', $lines[$j]->text)) {
+                  //  Gyazoの画像リンクを含む場合
+        
+                  //  例: 
+                  //  [https://gyazo.com/5598422019d8c545c0dfe26b620dcf28]
+        
+                  echo ("<p class=\"Gyazo\">");
+
+                  //  [https://gyazo.com/ が現れる位置
+                  $pos_gz_b = mb_strpos($lines[$j]->text, '[https://gyazo.com/');
+
+                  //  ] Gyazoの閉じかっこが現れる位置
+                  $pos_gz_e = mb_strpos($lines[$j]->text, ']');
+
+                  //  Gyazoのurl
+                  $gyazo_url = mb_substr($lines[$j]->text, $pos_gz_b + 1, $pos_gz_e - $pos_gz_b - 1);
+
+                  echo (mb_substr($lines[$j]->text, 0, $pos_gz_b));
+                  echo ("<br>");
+
+                  //  GyazoのAPIを叩いて、画像の埋め込みリンクを取得する
+                  // コネクションを開く
+                  $ch_gyazo = curl_init(); // はじめ
+        
+                  //オプション
+                  $gyazo_api = ("https://api.gyazo.com/api/oembed?url=" . $gyazo_url);
+                  curl_setopt($ch_gyazo, CURLOPT_URL, $gyazo_api);
+                  curl_setopt($ch_gyazo, CURLOPT_RETURNTRANSFER, true);
+                  $html_gyazo = curl_exec($ch_gyazo);
+
+                  //  画像を表示
+                  $gyazo_json = json_decode($html_gyazo);
+                  echo ("<img src=\"" . $gyazo_json->url . "\">");
+                  echo ("<br>");
+
+                  echo (mb_substr($lines[$j]->text, $pos_gz_e + 1, null));
+
+                  echo ("</p>");
+
+
+
+                } else if (preg_match('/\[.*\shttp\S*\]/', $lines[$j]->text)) {
+                  //  名前付きのリンク
+                  //  [リンク名 http...]
+        
+                  //  例: 
+                  //  あいうえお[link name https://www.php.net/manual/ja/function.strpos.php]これがリンク
+                  echo ("<p class=\"link-with-name\">");
+
+                  //  [が現れる位置
+                  $pos_bl = mb_strpos($lines[$j]->text, '[');
+
+                  //  http が現れる位置
+                  $pos_http = mb_strpos($lines[$j]->text, 'http');
+
+                  //  ] が現れる位置
+                  $pos_el = mb_strpos($lines[$j]->text, ']');
+
+
+                  echo (mb_substr($lines[$j]->text, 0, $pos_bl));
+                  echo (" <a href=\"");
+                  echo (mb_substr($lines[$j]->text, $pos_http, $pos_el - $pos_http));
+                  echo ("\" target=\"_blank\">");
+                  echo (mb_substr($lines[$j]->text, $pos_bl + 1, $pos_http - $pos_bl - 2));
+                  echo ("</a> ");
+                  echo (mb_substr($lines[$j]->text, $pos_el + 1, null));
+
+                } else if (preg_match('/http\S*\s/', $lines[$j]->text)) {
+                  //  名前のついていない、ただのurl
+        
+                  //  例
+                  //  あいうえお https://www.webdesignleaves.com/pr/php/php_basic_03.php これがリンク
+        
+                  echo ("<p class=\"normal-url\">");
+
+                  //  http が現れる位置
+                  $pos_http = mb_strpos($lines[$j]->text, 'http');
+
+                  //  ' ' (リンクの終わり)が現れる位置
+                  $pos_el = mb_strpos($lines[$j]->text, ' ', $pos_http);
+
+                  echo (mb_substr($lines[$j]->text, 0, $pos_http));
+                  echo (" <a href=\"");
+                  echo (mb_substr($lines[$j]->text, $pos_http, $pos_el - $pos_http));
+                  echo ("\" target=\"_blank\">link</a> ");
+                  echo (mb_substr($lines[$j]->text, $pos_el + 1, null));
+
+                } else if (preg_match('/http\S*/', $lines[$j]->text)) {
+                  //  名前のついていない、ただのurl(空白無しで終わるやつ)
+        
+                  //  例
+                  //  あいうえお https://www.webdesignleaves.com/pr/php/php_basic_03.php
+        
+                  echo ("<p class=\"normal-url2\">");
+
+                  //  http が現れる位置
+                  $pos_http = mb_strpos($lines[$j]->text, 'http');
+
+                  echo (mb_substr($lines[$j]->text, 0, $pos_http));
+                  echo (" <a href=\"");
+                  echo (mb_substr($lines[$j]->text, $pos_http, null));
+                  echo ("\" target=\"_blank\">link</a> ");
+
+                } else {
+
+                  echo ("<p>");
+                  echo ($lines[$j]->text);
+                  echo ("</p>");
+                }
+
+                $j++;
+
+              }
+              echo ("</details>");
+            }
             echo ("</div>\n");
           }
         }
